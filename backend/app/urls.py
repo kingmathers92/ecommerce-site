c2024992c6c454b0bbb53ad00b7a5fa884a74f0a
KHALED\E392 <khaledb.yahya@gmail.com>
2024-06-29 22:55:56 +0100

    fixed urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductFilter, StripeCheckoutView, getRoutes, registerUser, getProducts, getProduct, getUserProfile, getUsers, get_user_by_id
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import (
+    ProductFilter, StripeCheckoutView, getRoutes, registerUser,
+    getProducts, getProduct, getUserProfile, getUsers, get_user_by_id
+)
 
 urlpatterns = [
     path('', getRoutes, name="get-routes"),
@@ -13,8 +14,8 @@ urlpatterns = [
     path('products/<str:pk>', getProduct, name="get-product"),
     path('user/profile/', getUserProfile, name="get-user-profile"),
     path('users/', getUsers, name="get-users"),
-    path('<str:pk>/', get_user_by_id, name='get-user-id'),
-    path('search/', ProductFilter.as_view(), name="search-product"),
-    path('create-checkout-session/<pk>/',
+    path('users/<str:pk>/', get_user_by_id, name='get-user-id'),
+    path('products/search/', ProductFilter.as_view(), name="search-product"),
+    path('create-checkout-session/<str:pk>/',
          csrf_exempt(StripeCheckoutView.as_view()), name='checkout_session'),
 ]
